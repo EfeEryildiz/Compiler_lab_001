bb4e017ade7ce0e0f6cf4dab91e23e5dcf3db603
EfeEryildiz <yigitefem3@gmail.com>
2024-09-09 11:52:58 -0400

    new

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,57 @@
 package org.example;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PushbackReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    enum TOKEN { NUM, PLUS, EOF }
+    public static TOKEN scan() {
+
+        return TOKEN.EOF;
+    }
+
+
+    public static TOKEN main(String[] args) {
+
+        TOKEN nextToken;
+
+        try {
+            PushbackReader pushbackReader = new PushbackReader(new FileReader("input.txt"));
+
+            nextToken = scan();
+            while (nextToken != TOKEN.EOF) {
+                nextToken = scan();
+                }
+
+
+            System.out.println(nextToken);
+
+
+            int c = pushbackReader.read();
+
+            //Keep reading until eof
+            while (c != -1) {
+                System.out.println((char) c);
+
+                if (Character.isDigit(c))
+                    System.out.println("num");
+
+                else if (c == '+')
+                    return TOKEN.PLUS;
+
+                c = pushbackReader.read();
+
+        }
+
+    } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return nextToken;
     }
 }
